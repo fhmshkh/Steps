f98520412efe86f5c3c1ccb1d3de5c2073185aba
Mohd Faheem <connect.faheem7@gmail.com>
2024-08-24 12:52:07 +0530

    Finished

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import "./assets/index.css";
+const messages = [
+  "Learn React ⚛️",
+  "Apply for Jobs 💼",
+  "Invest your new Income 🤑",
+  "Enjoy vacation with Family 👪",
+  "Buy your favorite vehicle Mahindra THAR 🚙",
+];
+
+function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+
+  function handleNext() {
+    if (step < 5) setStep((s) => s + 1);
+  }
+
+  return (
+    <>
+      <h1 className="header">Steps</h1>
+
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+            <div className={`${step >= 4 ? "active" : ""}`}>4</div>
+            <div className={`${step >= 5 ? "active" : ""}`}>5</div>
+          </div>
+
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+
+      <footer className="footer">
+        © 2024
+        <a href="https://faheem7.netlify.app/" target="_blank">
+          Mohd Faheem....
+        </a>
+        All rights reserved.
+      </footer>
+    </>
+  );
+}
+
+export default App;
